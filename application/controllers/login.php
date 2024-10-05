71f79aa5d52024e38ccdb7ef4523a4a8ac61e69e
unknown <enyimber1999@gmail.com>
2024-10-05 12:13:34 -0500

    modificacion en login vista y en login controller

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,8 +2,16 @@
 
 class Login extends CI_Controller {
 
-	public function index()
-	{
+	public function __construct() {
+        parent::__construct();
+        $this->load->model('loginModel');
+    }
+
+	public function index(){
 		$this->load->view('loginVista');
+	}
+	
+	public function validarUsuario(){
+
 	}
 }
